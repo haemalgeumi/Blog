46ac966daea8b7f6575eb87d0e07fe106d24b22c
haemalgeumi <abk011@naver.com>
2024-02-07 21:27:15 +0900

    props를 사용해 모달제목 바꾸기

--- src/App.js
+++ src/App.js
@@ -8,47 +8,61 @@ function App() {
     "위에는 블로그",
     "밑에는 쇼핑몰",
   ]);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [modalClose, setModalClose] = useState(0);
+  let [modalTitle,setModalTitle] = useState(0);
+  function change() {
+    setListTitle(["기록하는 사람", "위에는 블로그", "밑에는 쇼핑몰"]);
+  }
 
   return (
     <div className="App">
       <div className="title">Yun Diary</div>
-      {
-      listTitle.map(function(title){
-        return(
-          <div className="list"onClick={() => {
-            setModal(!modal);
-          }}>
-           {title}<span
+      {listTitle.map(function (title, i) {
+        return (
+          <div
+            className="list"
+            onClick={() => {
+              setModal(!modal);
+              setModalTitle(i);
+            }}
+          >
+            {title}
+            <span
               onClick={() => {
-                setLike(like + 1);
+                let likeCopy = [...like];
+                likeCopy[i] = likeCopy[i] + 1;
+                setLike(likeCopy);
               }}
             >
               ❤
             </span>{" "}
-            {like}
-           <p>날짜</p>
-         </div>
-        )
-        
+            {like[i]}
+            <p>날짜</p>
+          </div>
+        );
       })}
-      {modal === true ? <Modal /> : null}
+      {modal === true ? <Modal listTitle={listTitle} change={change} modalTitle={modalTitle}/> : null}
     </div>
   );
 }
 
-function Modal() {
-  return (
-    <div className="modal-container">
-      <div className="modal-content">
-        <h1>제목</h1>
-        <p>내용</p>
-        <p>상세내용</p>
-      </div>
-    </div>
-  );
+function Modal(props) {
+  return(
+          <div className="modal-container">
+            <div className="modal-content">
+
+              <h1>{props.listTitle[props.modalTitle]}</h1>
+              <p>내용</p>
+              <p>상세내용</p>
+              <button onClick={props.change}>글수정</button>
+            </div>
+          </div>
+        
+      )
+  
 }
+
 
 export default App;
